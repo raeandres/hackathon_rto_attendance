a34449ea0da87b4eb788a256f72c52e3f221701c
Rae-An Andres <andrera@mfcgd.com>
2022-06-05 21:53:46 +0800

    Added Attendance controller

--- src/controllers/attendanceController.js
+++ src/controllers/attendanceController.js
@@ -1,28 +1,36 @@
 import mongoose from "mongoose";
-import { Schema } from "../models/attendanceModel";
+import { LoginSchema } from "../models/loginModel";
+import { EmployeeSchema } from "../models/employeeModel";
+import { AttendanceSchema } from "../models/attendanceModel";
+import { LocationSchema } from "../models/locationModel";
+import { ObjectId } from "mongodb";
 
 
-// const Attendance = mongoose.model('AttendanceSchema', Schema);
-// const Login = mongoose.model('LoginSchema', Schema);
-// const Employee = mongoose.model('EmployeeSchema', Schema);
+const Employee = mongoose.model('Employee', EmployeeSchema);
+const Attendance = mongoose.model('Attendance', AttendanceSchema);
+const Location = mongoose.model('Location', LocationSchema);
 
-// export const employeeLogin = (req, res) => {
-//     Login.findOne({
-//         email: req.body.email,
-//         password: req.body.password
-//     }, (err, login) => {
-//         if (err) {
-//             res.send(err);
-//         }
-//         else {
-//             if (login) {
-//                 console.log(login);
-//                 res.send(login);
-//             }
-//             else {
-//                 console.log('Invalid credentials');
-//                 res.send('Invalid credentials');
-//             }
-//         }
-//     });
-// }
+export const enterAttendance = (req, res) => {
+    // let employee = new Employee();
+    // employee.employee_id = req.body.employee_id;
+    // employee.employee_name = req.body.employee_name;
+    // employee.employee_designation = req.body.employee_designation;
+    // employee.employee_department = req.body.employee_department;
+    // employee.employee_email = req.body.employee_email;
+    // employee.employee_password = req.body.employee_password;
+    // employee.employee_mobile = req.body.employee_mobile;
+    // employee.employee_address = req.body.employee_address;
+    // employee.employee_joining_date = req.body.employee_joining_date;
+    // employee.employee_leaving_date = req.body.employee_leaving_date;
+    // employee.employee_status = req.body.employee_status;
+    // employee.created_date = new Date();
+    // employee.modified_date = new Date();
+    // employee.save((err, employee) => {
+    //     if (err) {
+    //         res.send(err);
+    //     }
+    //     else {
+    //         res.send(employee);
+    //     }
+    // });
+};
