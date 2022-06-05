10eb15a870861b87ce7f294fb31ae16e06b6406c
Rae-An Andres <andrera@mfcgd.com>
2022-06-05 21:52:29 +0800

    Added GET API with email param for employee endpoint

---
+++ src/controllers/employeeController.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import { EmployeeSchema } from "../models/employeeModel";
+
+
+const Employee = mongoose.model('Employee', EmployeeSchema);
+
+export const getEmployee = (req, res) => {
+    Employee.findOne(req.body.email, (err, employee) => {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            let response = {
+                "status": "success",
+                "message": "Login Successful",
+                "data": {
+                    employee_info: employee
+                }
+            }
+            res.json(response);
+        }
+    });
+}
+
