c09be6117f376977e7406594d02e2de24f352bca
Rae-An Andres <andrera@mfcgd.com>
2022-06-06 00:06:45 +0800

    Code cleanup

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -6,55 +6,31 @@ import { LoginSchema } from "../models/loginModel";
 const Login = mongoose.model('Login', LoginSchema);
 
 export const employeeLogin = (req, res) => {
-    // Login.findOne({
-    //     email: req.body.email,
-    //     password: req.body.password
-    // }, (err, login) => {
-    //     if (err) {
-    //         res.send(err);
-    //     }
-    //     else {
-    //         if (login) {
-    //             let response = {
-    //                 "status": "success",
-    //                 "message": "Login Successful",
-    //                 "data": {
-    //                     email: login.email,
-    //                     workday_id: login.workday_id,
-    //                 }
-    //             }
-    //             console.log(login);
-    //             res.send(response);
-    //         }
-    //         else {
-    //             console.log('Invalid credentials');
-    //             res.send('Invalid credentials');
-    //         }
-    //     }
-    // });
-
-    let login = new Login({
+    Login.findOne({
         email: req.body.email,
-        password: req.body.password,
-        workday_id: '312695487',
-        session: '123456789'
-    });
-
-    login.save((err, login) => {
+        password: req.body.password
+    }, (err, login) => {
         if (err) {
             res.send(err);
         }
         else {
-            let response = {
-                "status": "success",
-                "message": "Login Successful",
-                "data": {
-                    email: login.email,
-                    workday_id: login.workday_id,
+            if (login) {
+                let response = {
+                    "status": "success",
+                    "message": "Login Successful",
+                    "data": {
+                        email: login.email,
+                        workday_id: login.workday_id,
+                    }
                 }
+                console.log(login);
+                res.send(response);
             }
-            console.log(login);
-            res.send(response);
+            else {
+                console.log('Invalid credentials');
+                res.send('Invalid credentials');
+            }
         }
     });
+
 }
