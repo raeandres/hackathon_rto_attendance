ef16d666ee22ed07939860420883cf020ae290e6
Lyle Vince Dela Cuesta <lylevincedelacuesta@gmail.com>
2022-06-13 08:52:44 +0800

    Cachable UUID and modal

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -2,25 +2,26 @@
     import './App.css';
     import fingerprint from '@fingerprintjs/fingerprintjs';
     import moment from 'moment';
+    import Usage from './lib/Usage.svelte';
 
-    let date = moment().format('ddd MM/DD/y');
-    let clock = moment().format('HH:mm:ss');
+    let lastCheckInDate = localStorage.getItem('LAST_CHECKIN_DATE') ?? null;
+    let userFingerprint = JSON.parse(localStorage.getItem('FINGERPRINT')) ?? null;
+    let isCheckedIn = lastCheckInDate === moment().format('MM/DD/YYYY');
+
+    let date = moment().format('ddd MM/DD/YYYY');
+    let clock = moment().format('HH:mm:ss A');
+
     let payload = {
-        email: '',
-        uuid: '',
+        email: userFingerprint?.email ?? '',
+        uuid: userFingerprint?.uuid ?? '',
     };
-    let submitting = false;
-    let result;
 
-    let uuid = fingerprint
-        .load()
-        .then(res => res.get())
-        .then(res => res.visitorId);
+    let result;
+    let submitting = false;
 
     async function checkIn() {
         const url = import.meta.env.DEV ? `${import.meta.env.VITE_API_URL}/checkin` : '/api/checkin';
-        console.log(url);
-        payload.uuid = await uuid;
+        payload.uuid = await getFingerprint();
         submitting = true;
 
         const res = await fetch(url, {
@@ -31,44 +32,72 @@
             body: JSON.stringify(payload),
         });
 
+        if (res.status === 200) {
+            userFingerprint = payload;
+            localStorage.setItem('LAST_CHECKIN_DATE', moment().format('MM/DD/YYYY'));
+            localStorage.setItem('FINGERPRINT', JSON.stringify(userFingerprint));
+            isCheckedIn = true;
+        }
+
         submitting = false;
         result = await res.json();
     }
 
+    function getFingerprint() {
+        return fingerprint
+            .load()
+            .then(res => res.get())
+            .then(res => res.visitorId);
+    }
+
     $: {
         setInterval(() => {
-            clock = moment().format('HH:mm:ss');
+            clock = moment().format('HH:mm:ss A');
         }, 1000);
     }
 </script>
 
+<Usage />
 <nav class="navbar bg-neutral p-0 m-0">
     <a href="/"
         ><img class="h-[64px]" src="/images/manulife-logo-only.svg" alt="manulife-logo-only" />
         <img class="h-[24px] ml-5" src="/images/manulife-text-only.svg" alt="manulife-text-only" />
     </a>
 </nav>
-<main class="max-w-md mx-auto w-full p-6 py-16">
-    {#await uuid then value}
-        <div class="flex flex-col gap-4">
-            <div class="flex flex-col gap-2">
-                <h1 class="text-8xl font-bold">{clock}</h1>
+<main class="max-w-md mx-auto w-full p-6 py-32">
+    {#if !isCheckedIn}
+        <div class="flex flex-col">
+            <div class="flex flex-col gap-2 items-center mb-16">
+                <h1 class="text-5xl font-bold">{clock}</h1>
                 <h3 class="text-lg font-semibold">{date}</h3>
             </div>
-            <div class="form-control w-full">
-                <label class="label" for="">
-                    <span class="label-text">Email (MFCGD)</span>
-                </label>
-                <input
-                    type="text"
-                    placeholder="mitdc@mfcgd.com"
-                    class="input input-bordered w-full"
-                    bind:value={payload.email}
-                />
-            </div>
-            <button on:click={checkIn} class="btn btn-lg btn-secondary w-full" class:loading={submitting}>
+            {#if !userFingerprint}
+                <div class="form-control w-full">
+                    <label class="label" for="">
+                        <span class="label-text">Email (MFCGD)</span>
+                    </label>
+                    <input
+                        type="text"
+                        placeholder="mitdc@mfcgd.com"
+                        class="input input-bordered w-full"
+                        bind:value={payload.email}
+                    />
+                </div>
+            {:else}
+                <h2 class="text-center text-primary">Username: {payload.email}</h2>
+            {/if}
+
+            <button on:click={checkIn} class="btn btn-lg btn-secondary w-full mt-4" class:loading={submitting}>
                 Check-In
             </button>
         </div>
-    {/await}
+    {:else}
+        <div class="flex flex-col gap-4 items-center">
+            <img src="/images/undraw_confirmed.svg" alt="confirmed" />
+            <div class="text-center">
+                <h1 class="text-xl font-bold text-primary">Success!</h1>
+                <h3 class="text-lg text-primary">You have already checked-in for today.</h3>
+            </div>
+        </div>
+    {/if}
 </main>
---
+++ client/src/lib/Usage.svelte
@@ -0,0 +1,28 @@
+<script>
+    let open = true;
+</script>
+
+{#if open}
+    <div class="modal modal-open">
+        <div class="modal-box">
+            <h3 class="font-bold text-lg">USAGE & INFORMATION</h3>
+            <div class="modal-body py-4">
+                <p>The application uses the following to determine and validate your attendance:</p>
+                <ul class="list-disc pl-6 mt-2">
+                    <li>UUID (Browser Generated ID)</li>
+                    <li>Location</li>
+                </ul>
+                <p class="mt-4">
+                    If not necessary, try to <span class="font-bold text-error"
+                        >avoid clearing cache and changing browser</span
+                    >
+                    for this application as there is a small chance that your UUID will be regenerated and
+                    <span class="font-bold text-error"> might result checkin failure.</span>
+                </p>
+            </div>
+            <div class="modal-action">
+                <button on:click={() => (open = false)} class="btn btn-primary w-full">GOT IT!</button>
+            </div>
+        </div>
+    </div>
+{/if}
